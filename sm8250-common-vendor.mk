479f8f70161d89051ba59c11ca3dc6c2358906c8
bijoyv9 <bijoyv9@gmail.com>
2025-08-17 07:28:26 +0000

    sm8250-common: Pull libarcsoft_dualcam blobs from OnePlus 12
    libarcsoft_dualcam_refocus_left.so from newer device
    fixes oplus camera crashes on rear portrait photo mode.
    We might as well pull the similar blobs and match them
    with the same build that oplus camera is sourced from.
    
    Change-Id: I4c925c0a9a9435bbc7c034d379189a66ef82c7a5

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -1045,6 +1045,7 @@ PRODUCT_PACKAGES += \
     libapsexif \
     libapsjpeg \
     libapspng \
+    libarc.ion \
     libarcsoft_aicolor_image \
     libarcsoft_aicolor_video \
     libarcsoft_distortion_correction \
@@ -1069,6 +1070,7 @@ PRODUCT_PACKAGES += \
     libarcsoft_portraitSN_hvx_stub \
     libarcsoft_portrait_super_night_raw \
     libarcsoft_preview_antibanding \
+    libarcsoft_qnnhtp \
     libarcsoft_scbokeh_image \
     libarcsoft_scbokeh_video \
     libarcsoft_smart_denoise \
